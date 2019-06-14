e7c63bd4be2127d63071de2b101909c0bc016927
kouwq <kouwq@smail.swufe.edu.cn>
2019-06-15 01:48:42 +0800

    6/15 renew layout addClearListener

--- app/src/main/java/com/demo/hello/seamates/LoginActivity.java
+++ app/src/main/java/com/demo/hello/seamates/LoginActivity.java
@@ -3,23 +3,34 @@ package com.demo.hello.seamates;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
     private final String TAG = "LoginActivity";
     EditText username, pwd;
     Button btn1, btn2;
+    ImageView imageView1, imageView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
         username = findViewById(R.id.et_log_u);
         pwd = findViewById(R.id.et_pwd_u);
+        imageView1 = findViewById(R.id.iv_unaClear);
+        imageView2 = findViewById(R.id.iv_pwdClear);
+        //添加清除监听
+        addClearListener(username,imageView1);
+        addClearListener(pwd,imageView2);
+
         btn1 = findViewById(R.id.log_btn1);
         btn1.setOnClickListener(this);
         btn2 = findViewById(R.id.log_btn2);
@@ -61,4 +72,38 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     public void onPointerCaptureChanged(boolean hasCapture) {
 
     }
+
+    public static void addClearListener(final EditText editText, final ImageView imageView) {
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                //如果有输入内容就显示clear按钮
+                String str = s + "";
+                if (str.length() > 0) {
+                    imageView.setVisibility(View.VISIBLE);
+                } else {
+                    imageView.setVisibility(View.INVISIBLE);
+                }
+
+            }
+        });
+        imageView.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                editText.setText("");
+            }
+        });
+    }
+
 }
